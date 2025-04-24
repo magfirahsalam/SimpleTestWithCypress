7a332ddc2da2206034ecc7a283e6e2dfe247c77c
Magfirah Salam <43084693+magfirahsalam@users.noreply.github.com>
2025-04-24 11:11:48 +0700

    Logout Test

---
+++ cypress/e2e/logout.cy.js
@@ -0,0 +1,43 @@
+describe("Logout Account", () => {
+    it("Should logout successfullt", () => {
+        cy.visit("https://partner.hpe.com/");
+        cy.get("#p_p_id_loginPortlet_", 
+            { timeout: 10000 })
+            .should('be.visible');
+        cy.get("header > span", 
+            { timeout: 10000 })
+            .should('be.visible');
+        cy.get("#oktaEmailInput")
+            .should('be.visible')
+            .click();
+        cy.get("#oktaEmailInput")
+            .type("demo_apj_distributor@pproap.com");
+        cy.get("#oktaSignInBtn")
+            .should('not.have.class', 'disabled')
+            .click();
+        cy.get("#password-sign-in", 
+                { timeout: 10000 })
+                .should('be.visible')
+                .click();
+        cy.get("#password-sign-in")
+                .type("Login2PRP!");
+        cy.get("#onepass-submit-btn").click();
+        cy.origin('https://login-iam.ext.hpe.com', () => {
+        cy.get("input[value='Log on']",
+            { timeout: 10000 })
+            .should('be.visible');
+            });
+        cy.visit("https://partner.hpe.com");
+        cy.xpath("//a[@id='userIconImg']//img", 
+            { timeout: 10000 })
+            .should("be.visible"); 
+        cy.get("#userIconImg > img")
+        .click();
+        cy.xpath("//a[@href='/c/portal/logout']" )
+            .should('be.visible')
+        cy.get("li.logOut span")
+            .click();
+        cy.location("href")
+            .should("include", "https://partner.hpe.com/web/prp");
+        });
+  });
